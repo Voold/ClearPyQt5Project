9fbb8a787d24831764821c5e469e7c02acecd617
Voold <barsik.koteevich@mail.ru>
2024-02-02 06:05:42 +0700

    Первый коммит для этого проекта. К данному моменту реализован графический интерфейс и ряд самых базовых функций. Кнопкам присвоен их функционал, пока что работа осуществляется только в файле main.py, из за сложностей с реализацией внешних функций Программа сейчас на 1 ступени. Далее планирую занятся реализацией оставшихся переключателей и начать описывать класс Barrel (ключевой класс программы)

---
+++ functions.py
@@ -0,0 +1,14 @@
+from main import *
+
+#
+# def mCl_click(dict, name, obj):
+#     val = name
+#     if dict[val] == 0:
+#         dict[val] = 1
+#     else:
+#         dict[val] = 0
+#     exec(f"{obj}.setText('1')")
+
+    # exec(f"{obj}.setText('1')")
+
+    # exec(f"{name.objectName}_status.setText(str({dict}.get('{name}')))")
